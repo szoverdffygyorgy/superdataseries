1d2ab2bbf499651a3612183630a9ee3c562168db
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2017-05-07 00:08:49 +0200

    no message

--- src/es6example.js
+++ src/es6example.js
@@ -7,3 +7,23 @@ function aFunction(params) {
 let aFunction = (params) => {
   //do something here!
 }
+
+function getProcessedData(url) {
+  return downloadData(url) // returns a promise
+    .catch(e => {
+      return downloadFallbackData(url); // returns a promise
+    })
+    .then(v => {
+      return processDataInWorker(v); // returns a promise
+    });
+}
+
+async function getProcessedData(url) {
+  let v;
+  try {
+    v = await downloadData(url);
+  } catch(e) {
+    v = await downloadFallbackData(url);
+  }
+  return processDataInWorker(v);
+}
